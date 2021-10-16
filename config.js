7748c79261793b5d2c38027ac8beb127e9a7df3c
Charles Liu <charles.liu0309@gmail.com>
2021-10-16 03:15:06 -0400

    deployed to mumbai network

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftAddress = '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707';
-export const nftMarketAddress = '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9';
+export const nftAddress = '0xDbb05A2A7185dA21cb4Ca732485dC5BD52864e63';
+export const nftMarketAddress = '0x4AaA996275308c0ca8c9120170FDc069e82d1992';
